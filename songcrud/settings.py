d14646196a50a8595c2877cd3ba2a349c88aa0bc
sangkips <sangkipkoech@gmail.com>
2022-10-10 21:59:59 +0300

    added CORS headers

--- songcrud/settings.py
+++ songcrud/settings.py
@@ -40,9 +40,12 @@ INSTALLED_APPS = [
 
     'musicapp',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    "http://127.0.0.1:3000",
 ]
 
 ROOT_URLCONF = 'songcrud.urls'
